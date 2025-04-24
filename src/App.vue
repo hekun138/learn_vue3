ff85c724a2b16d667302faa912c21f734ae4cb3a
hogan <418730859@qq.com>
2025-04-24 19:13:32 +0800

    vue2组件方式

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
-    <h1>Hello World</h1>
+    <div class="app">
+        <h1>Hello World</h1>
+        <Person />
+    </div>
 </template>
 
-<script setup>
-
+<script lang="ts">
+import Person from './components/Person.vue'
+export default {
+    name: 'App',
+    components: {
+        Person
+    }
+}
 </script>
 
+<style scoped>
+.app {
+    background-color: #f0f0f0;
+    padding: 20px;
+    border-radius: 10px;
+}
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改姓名</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+    </div>
+</template>
+
+
+<script lang="ts">
+    export default {
+        name: 'Person',
+        data() {
+            return {
+                name: '张三',
+                age: 18,
+                gender: '男'
+            }
+        },
+        methods: {
+            showTel() {
+                alert('查看联系方式')
+            },
+            changeName() {
+                this.name = '李四'
+            },
+            changeAge() {
+                this.age += 1;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.person {
+    background-color: skyblue;
+    padding: 20px;
+    border-radius: 10px;
+}
+button {
+    margin-right: 10px;
+}
+</style>
